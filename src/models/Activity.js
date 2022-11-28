1d279ee200c352f2b2fab412eb91f66c40b86f74
Markanight <marcosvila96@gmail.com>
2022-11-28 17:31:26 -0300

    actualizando model activity

--- src/models/Activity.js
+++ src/models/Activity.js
@@ -17,11 +17,11 @@ module.exports = (sequelize) => {
       allowNull: false
     },
     duration: {
-      type: DataTypes.ENUM('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24','25','26','27','28','29','30'),
+      type: DataTypes.INTEGER,
       allowNull: false
     },
     season: {
-      type: DataTypes.ENUM('Summer', 'Autumn', 'Winter', 'Spring'),
+      type: DataTypes.STRING,
       allowNull: false
     }
   });
