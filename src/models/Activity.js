4d6e0dc32b4c6cb787d1c6f067f54ec82649c507
Markanight <marcosvila96@gmail.com>
2022-11-30 11:45:04 -0300

    updating duration

--- src/models/Activity.js
+++ src/models/Activity.js
@@ -17,7 +17,7 @@ module.exports = (sequelize) => {
       allowNull: false
     },
     duration: {
-      type: DataTypes.ENUM('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'),
+      type: DataTypes.INTEGER,
       allowNull: false
     },
     season: {
